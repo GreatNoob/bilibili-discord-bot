41286e101c94e88489b04c19c7ba35dc18faf63c
Jingyu Ding <dingjingyu233@gmail.com>
2022-01-09 18:03:32 +0800

    updated package info

--- lib/bilibili_player.ts
+++ lib/bilibili_player.ts
@@ -1,6 +1,6 @@
 import * as request from "superagent"
 
-function get_video_info(){
+export function get_video_info(){
     request
         .get("https://www.baidu.com/")
         .end((res) => {
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import {get_video_info} from "../lib/bilibili_player"
+
+function main(){
+    get_video_info();
+}
+
+main()
